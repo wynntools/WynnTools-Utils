27c80f124c88becad27da9d6e8ae3fde39a7beb8
Kath <Kathund@users.noreply.github.com>
2023-09-07 16:55:57 +0800

    Better Activity Status

--- src/scripts/activityStatus.js
+++ src/scripts/activityStatus.js
@@ -1,11 +1,8 @@
 const { scriptMessage, errorMessage } = require('../functions/logger.js');
 const { generateID } = require('../functions/helper.js');
-const packageJson = require('../../package.json');
 const { ActivityType } = require('discord.js');
 const config = require('../../config.json');
 const cron = require('node-cron');
-const path = require('path');
-const fs = require('fs');
 
 let timezoneStuff = null;
 if (config.other.timezone == null) {
@@ -15,18 +12,10 @@ if (config.other.timezone == null) {
 }
 
 var num = 0;
-const commands = [];
-fs.readdirSync(path.resolve(__dirname, '../commands/general')).forEach((file) => {
-  if (!file.endsWith('.js')) return;
-  commands.push(file);
-});
-client.user.setPresence({ activities: [{ name: 'to crys', type: ActivityType.Listening }] });
+client.user.setPresence({ activities: [{ name: 'Over Tickets', type: ActivityType.Watching }] });
 var activities = [
-  { id: 'servers', title: `over ${client.guilds.cache.size} servers!`, type: 'Watching' },
-  { id: 'commands', title: `${commands.length} commands!`, type: 'Watching' },
-  { id: 'totalCommands', type: 'Listening' },
-  { id: 'users', type: 'Watching' },
-  { id: 'version', title: `version ${packageJson.version}!`, type: 'Playing' },
+  { id: 'ticket', title: 'Over Tickets', type: 'Watching' },
+  { id: 'support', title: 'Over The Support Server', type: 'Watching' },
 ];
 cron.schedule(
   '*/5 * * * *',
@@ -34,34 +23,9 @@ cron.schedule(
     try {
       if (config.other.devMode) return scriptMessage('Dev mode enabled - not changing activity status');
       scriptMessage(`Changing activity status - ${activities[num].id}`);
-      let userData;
-      let totalCommandsRun;
-      let totalUsers;
-      if (activities[num].id === 'totalCommands') {
-        userData = JSON.parse(fs.readFileSync('data/userData.json'));
-        totalCommandsRun = 0;
-        for (const entry in userData) {
-          totalCommandsRun += userData[entry].commandsRun;
-        }
-        client.user.setPresence({
-          activities: [
-            {
-              name: `to ${totalCommandsRun} Total Commands Run`,
-              type: ActivityType[activities[num].type],
-            },
-          ],
-        });
-      } else if (activities[num].id === 'users') {
-        userData = Object.keys(JSON.parse(fs.readFileSync('data/userData.json')));
-        totalUsers = userData.length;
-        client.user.setPresence({
-          activities: [{ name: `${totalUsers} Total Users! `, type: ActivityType[activities[num].type] }],
-        });
-      } else {
-        client.user.setPresence({
-          activities: [{ name: activities[num].title, type: ActivityType[activities[num].type] }],
-        });
-      }
+      client.user.setPresence({
+        activities: [{ name: activities[num].title, type: ActivityType[activities[num].type] }],
+      });
       num++;
       if (num == activities.length) num = 0;
     } catch (error) {
